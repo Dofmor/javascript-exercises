7acf8c8e64dfa8690c6256f811c7da49456ce0b8
dofmor <seneca.19070@gmail.com>
2024-07-28 05:52:33 -0700

    Finished exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    n = Number (n)
+
+    if (n < 0)
+        return 'OOPS'
+    if (n == 0)
+        return 0
+
+    
+    let firstNum = 0, secondNum = 1
+    let newNum
+    for (let i = 2; i <= n; i++) {
+        newNum = firstNum + secondNum
+        firstNum = secondNum
+        secondNum = newNum
+    }
+    return secondNum
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
